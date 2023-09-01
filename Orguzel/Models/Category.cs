b0c5a486afbe196efe041e80c871a6580a97b756
salim.kartal <salimkartal@outlook.com.tr>
2023-09-01 14:22:30 +0300

    Add project files.

---
+++ Orguzel/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orguzel.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
